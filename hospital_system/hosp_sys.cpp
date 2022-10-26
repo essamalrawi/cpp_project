11c51ab8b288cda6210e951edc5bb53076b9b632
Essam Al-Rawi <77260050+essamalrawi@users.noreply.github.com>
2022-10-26 16:07:50 +0200

    Update hosp_sys.cpp

--- hospital_system/hosp_sys.cpp
+++ hospital_system/hosp_sys.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-std::string septation2[15], septation2[15], septation3[15], septation4[15], septation5[15],septation6[15], septation7[15], septation8[15], septation9[15], septation10[15], septation11[15], septation12[15], septation13[15], septation14[15], septation15[15], septation16[15], septation17[15], septation19[15], septation20[15];
+std::string septation1[15], septation2[15], septation2[15], septation3[15], septation4[15], septation5[15],septation6[15], septation7[15], septation8[15], septation9[15], septation10[15], septation11[15], septation12[15], septation13[15], septation14[15], septation15[15], septation16[15], septation17[15], septation19[15], septation20[15];
 int menu () {
     
     std::cout << "1) Add new patient\n";
